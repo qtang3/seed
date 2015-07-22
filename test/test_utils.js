41b17d207beb7ed1833b232944079640f20181ab
Quan Tang <qtang@esri.com>
2015-07-22 16:45:54 -0700

    fix build

--- test/test_utils.js
+++ test/test_utils.js
@@ -2,6 +2,6 @@ var utils = require("../src/utils"),
 	assert = require("should");
 describe("test utils", function() {
 	it("test double()", function() {
-		utils.double(3).should.equal(19);
+		utils.double(3).should.equal(9);
 	})
 })
